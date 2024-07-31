4dae971700dd2ec2bccc212b5cf326efe2e07354
Anirudha B Shetty <ab007shetty@gmail.com>
2024-07-31 07:54:38 +0530

    Making project serverless

--- api/playlists.js
+++ api/playlists.js
@@ -1,22 +1,18 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
 
-const dbPath = path.join(__dirname, '../database/vimusic.db');
-const db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error('Error opening database', err);
-  } else {
-    console.log('Connected to SQLite database');
-  }
-});
+    import { createConnection } from 'sqlite3';
+    import { join } from 'path';
 
-export default (req, res) => {
-  const sql = 'SELECT * FROM Playlist'; // Adjust the table name if different
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
+    export default function handler(req, res) {
+      const dbPath = join(process.cwd(), '../database/vimusic.db');
+      const db = new createConnection(dbPath);
+
+      const sql = 'SELECT * FROM Playlist';
+      db.all(sql, [], (err, rows) => {
+        if (err) {
+          res.status(400).json({ error: err.message });
+          return;
+        }
+        res.json({ playlists: rows });
+      });
     }
-    res.json({ playlists: rows });
-  });
-};
+    
